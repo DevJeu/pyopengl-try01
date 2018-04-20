de1372378cd1362bdc0b03b6b3cf6f3da76a9edf
Steve Webb <bigwebb@gmail.com>
2018-04-20 10:27:11 -0600

    reverted back to working cube

--- cube.py
+++ cube.py
@@ -78,9 +78,7 @@ def main():
 
     gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
 
-    glTranslatef(0,0, -10)
-
-    glRotatef(25, 2, 1, 0)
+    glTranslatef(0.0,0.0, -5)
 
     while True:
         for event in pygame.event.get():
@@ -88,26 +86,11 @@ def main():
                 pygame.quit()
                 quit()
 
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    glTranslatef(-0.5,0,0)
-                if event.key == pygame.K_RIGHT:
-                    glTranslatef(0.5,0,0)
-
-                if event.key == pygame.K_UP:
-                    glTranslatef(0,1,0)
-                if event.key == pygame.K_DOWN:
-                    glTranslatef(0,-1,0)
-
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 4:
-                    glTranslatef(0,0,1.0)
-
-                if event.button == 5:
-                    glTranslatef(0,0,-1.0)
-
         glRotatef(1, 3, 1, 1)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
         Cube()
         pygame.display.flip()
         pygame.time.wait(10)
+
+
+main()
